f9b2cef72e3350c93affa2187b44a14e153047b1
Giada Delli <g.delli@techgap.it>
2025-05-14 10:39:28 +0200

    Added buttons to dialog spese

--- public/situazione-attuale.js
+++ public/situazione-attuale.js
@@ -36,6 +36,7 @@ const btnUscita = document.getElementById("btn-apri-dialog-uscita");
 const dialogUscita = document.getElementById('dialog-uscita');
 
 btnUscita?.addEventListener('click', () => dialogUscita.showModal());
+document.getElementById('btn-chiudi-dialog-uscita')?.addEventListener('click', () => dialogUscita.close());
 
 // Nuova entrata
 const btnEntrata = document.getElementById('btn-apri-dialog-entrata');
